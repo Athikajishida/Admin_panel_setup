3797036034b1685faae30d96d88d1b7bfaed3ca6
Athika Jishida <athikajishida@gmail.com>
2023-12-09 11:15:12 +0530

    init commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,36 @@
+class ApplicationController < ActionController::Base
+
+    private
+  
+    def authenticate_user!
+      redirect_to root_path, alert: 'You must be logged in to do that' unless user_signed_in?
+    end
+  
+    def current_user
+      Current.user ||= authenticate_user_from_session
+    end
+    helper_method :current_user
+  
+    def authenticate_user_from_session
+        user_id = session[:user_id]
+        puts "User ID from session: #{user_id}"
+        User.find_by(id: user_id)
+      end
+      
+  
+    def user_signed_in?
+      current_user.present?
+    end
+    helper_method :user_signed_in?
+  
+    def login(user)
+      Current.user = user
+      reset_session
+      session[:user_id] = user.id
+    end
+  
+    def logout(user)
+      Current.user = nil
+      reset_session
+    end
+  end
